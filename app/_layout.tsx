e07053d20a762355e0864d5547554c67a25ffcd3
DannyHOKK <dannyhkk102@gmail.com>
2025-06-17 18:02:56 +0800

    fix bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
+import { AuthProvider } from "@/contexts/AuthContext";
 import { Stack } from "expo-router";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="testing"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </AuthProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,51 @@
-import { Text, View } from "react-native";
+import { useAuth } from "@/contexts/AuthContext";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { Redirect, useRouter } from "expo-router";
 
 export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="color-slate-800">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
-  );
+  // const [userInfo, setUserInfo] = useState({});
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  GoogleSignin.configure({
+    webClientId:
+      "951699356369-e3tfu189adfpkfjf73bh58nt34dj7g5j.apps.googleusercontent.com", // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+    scopes: ["email", "profile"], // what API you want to access on behalf of the user, default is email and profile
+    offlineAccess: false, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    forceCodeForRefreshToken: false, // [Android] related to `serverAuthCode`, read the docs link below *.
+    iosClientId:
+      "951699356369-ve77irls7r4n8i69nk18ejrg3usg06dl.apps.googleusercontent.com", // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  });
+
+  // const signIn = async () => {
+  //   try {
+  //     await GoogleSignin.hasPlayServices();
+  //     const response = await GoogleSignin.signIn();
+
+  //     console.log(response);
+  //     console.log("hi");
+  //     if (isSuccessResponse(response)) {
+  //       setUserInfo(response.data);
+  //     } else {
+  //       // sign in was cancelled by user
+  //     }
+  //   } catch (error) {
+  //     if (isErrorWithCode(error)) {
+  //       switch (error.code) {
+  //         case statusCodes.IN_PROGRESS:
+  //           // operation (eg. sign in) already in progress
+  //           break;
+  //         case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+  //           // Android only, play services not available or outdated
+  //           break;
+  //         default:
+  //         // some other error happened
+  //       }
+  //     } else {
+  //       // an error that's not related to google sign in occurred
+  //     }
+  //   }
+  // };
+
+  return <Redirect href={isAuthenticated ? "/(tabs)/index" : "/(auth)"} />;
 }
